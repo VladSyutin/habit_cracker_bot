677d163d8f054c66d8f096b5de5eb9ce0c4c728e
Владислав Сютин <vladislavsyutin@MacBook-Air-Vladislav.local>
2025-06-29 11:36:12 +0300

    Исправлен токен на dev-бота

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Временно используем токен напрямую
-BOT_TOKEN = "7848619890:AAGoF1eRzXpjndo22ctK2gMKLQHHaf5v7Jc"
+BOT_TOKEN = "8130970302:AAFL7Ud0C_KR15iVBzuNpep-Bwq2dpc8IY4"
 # BOT_TOKEN = os.getenv('BOT_TOKEN')
 DATABASE_PATH = 'habits.db' 
